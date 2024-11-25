ec75ae51933b7f1b48a08d659b24f9c45017981c
maria eduarda <escolamadu9@gmail.com>
2024-11-25 10:48:24 -0300

    Implementação inicial do cálculo da média

---
+++ media_alunos.py
@@ -0,0 +1,12 @@
+# media_alunos.py
+
+# Entrada
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Processo
+media = (nota1 + nota2 + nota3) / 3
+
+# Saída
+print(f"A média do aluno é: {media:.2f}")
